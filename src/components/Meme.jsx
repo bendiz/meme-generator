cbe3d051fd62cbb2924a978d4b44864cd7f608d7
Bendiz <benerok@icloud.com>
2023-03-06 14:09:54 +0100

    Make it possible to add text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,36 +3,59 @@ import './Meme.css';
 import memeData from './memeData';
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState('');
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
 
   const getMemeImage = (event) => {
     event.preventDefault();
     const memesArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    setMeme((prev) => ({
+      ...prev,
+      randomImage: memesArray[randomNumber].url,
+    }));
   };
+
+  const handleChange = (event) => {
+    let { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
+  console.log(meme);
 
   return (
     <div className="Meme">
-      <form action="" method="get" className="form">
+      <form action="" className="form">
         <div className="text-input">
           <input
             type="text"
-            name="firstline"
-            id="firstline"
+            name="topText"
+            id="topText"
             placeholder="First line"
+            value={meme.topText}
+            onChange={handleChange}
           />
           <input
             type="text"
-            name="secondline"
-            id="secondline"
+            name="bottomText"
+            id="bottomText"
             placeholder="Second line"
+            value={meme.bottomText}
+            onChange={handleChange}
           />
         </div>
-        <button type="submit" onClick={getMemeImage}>
-          Get a new meme image 🖼️
-        </button>
-        <img src={memeImage} alt="A funny meme" className="image" />
+        <button onClick={getMemeImage}>Get a new meme image 🖼️</button>
+        <div className="meme-container">
+          <img src={meme.randomImage} alt="A funny meme" className="image" />
+          <h2 className="top text">{meme.topText}</h2>
+          <h2 className="bottom text">{meme.bottomText}</h2>
+        </div>
       </form>
     </div>
   );
